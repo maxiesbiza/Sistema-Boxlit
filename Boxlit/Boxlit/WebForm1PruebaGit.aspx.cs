e0050f9cd956b8e5f58ff4c17abc4fd46b698b11
Maxi <maxiesbiza.esbiza@gmail.com>
2019-08-03 11:24:13 -0300

    Second Commit

---
+++ Boxlit/Boxlit/WebForm1PruebaGit.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Boxlit
+{
+    public partial class WebForm1PruebaGit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
