6a7fb91276d8adabc63ce8134ede32d4c4635e44
Gonzalo Barrientos <gnz.barrientos@hotmail.com>
2019-08-14 03:02:54 -0300

    Panel Admin

--- Boxlit/Boxlit/Admin.aspx
+++ Boxlit/Boxlit/Admin.aspx
@@ -1,2 +1,11 @@
 ﻿<%@ Page Title="Panel de Administrador" Language="C#" MasterPageFile="~/plantillas/PanelAdmin.Master" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="Boxlit.Admin" %>
-
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+<body>
+    <!--Script para no poder volver atras la pagina-->
+    <script Language="JavaScript">
+    if(history.forward(1)){
+    history.replace(history.forward(1));
+    }
+    </script>
+</body> 
+</asp:Content>
--- Boxlit/Boxlit/plantillas/PanelAdmin.Master.designer.cs
+++ Boxlit/Boxlit/plantillas/PanelAdmin.Master.designer.cs
@@ -11,5 +11,41 @@ namespace Boxlit.plantillas {
     
     
     public partial class PanelAdmin {
+        
+        /// <summary>
+        /// Control head.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
+        /// Control regionTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder regionTitulo;
+        
+        /// <summary>
+        /// Control regionContenido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder regionContenido;
+        
+        /// <summary>
+        /// Control regionScript.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder regionScript;
     }
 }
