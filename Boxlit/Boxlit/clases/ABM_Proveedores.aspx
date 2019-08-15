d197ac9b3c2344aecf13340ce2f64437d5ef061b
Maxi <maxiesbiza.esbiza@gmail.com>
2019-08-15 15:37:01 -0300

    Agregado ABM Proveedores

---
+++ Boxlit/Boxlit/clases/ABM_Proveedores.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/plantillas/PanelAdmin.Master" AutoEventWireup="true" CodeBehind="ABM_Proveedores.aspx.cs" Inherits="Boxlit.clases.ABM_Proveedores" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+   <style>
+        .estilo{
+            margin: 20px 20px 0px 20px;
+        }
+        .center{
+            margin:auto;
+            width: 100%;
+            text-align:center;
+            background-color:burlywood;
+            margin-top: 20px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="regionTitulo" runat="server"></asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="regionContenido" runat="server"></asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="regionScript" runat="server">
+    <H2>ABM Proveedores</H2>
+    <div class ="center">
+        <asp:Button ID="btnMostrarPanelNuevoProv" runat="server" Text="Agregar un nuevo proveedor" OnClick="btnMostrarPanelNuevoProv_Click" />
+        <asp:Panel ID="PanelNuevoProveedor" runat="server" Visible="false">
+            Nombre de la Empresa Proveedora<br />
+            <asp:TextBox ID="txtNombreProv" runat="server"></asp:TextBox><br />
+            <br />
+            Nombre del contacto de la empresa<br />
+            <asp:TextBox ID="txtContactoNombre" runat="server"></asp:TextBox><br />
+            <br />
+            CUIT<br />
+            <asp:TextBox ID="txtCUIT" runat="server"></asp:TextBox><br />
+            <br />
+            Pais<br />
+            <asp:TextBox ID="txtPais" runat="server"></asp:TextBox><br />
+            <br />
+            Ciudad<br />
+            <asp:TextBox ID="txtCiudad" runat="server"></asp:TextBox><br />
+            <br />
+            Telefono<br />
+            <asp:TextBox ID="txtTelefono" runat="server"></asp:TextBox><br />
+            <br />
+            <asp:Button ID="btnMinimizar" runat="server" Text="Minimizar" OnClick="btnMinimizar_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnAgregar" runat="server" Text="Agregar" OnClick="btnAgregar_Click" ValidationGroup="registro"/>
+
+        
+        </asp:Panel>
+
+    </div>
+    
+
+    <div class="center">
+        <asp:GridView ID="GridViewProveedores" runat="server" OnSelectedIndexChanged="GridViewProveedores_SelectedIndexChanged" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="ProveedorID" HeaderText="ProvID" />
+                <asp:BoundField DataField="ProveedorNombre" HeaderText="Nombre Proveedor" />
+                <asp:BoundField DataField="ContactoNombre" HeaderText="Nombre Contacto" />
+                <asp:BoundField DataField="CUIT" HeaderText="CUIT" />
+                <asp:BoundField DataField="Pais" HeaderText="Pais" />
+                <asp:BoundField DataField="Ciudad" HeaderText="Ciudad" />
+                <asp:BoundField DataField="Telefono" HeaderText="Telefono" />
+                <asp:ButtonField CommandName="Select" Text="Seleccionar" ButtonType="Button" />
+            </Columns>
+        </asp:GridView>
+    </div>
+    
+    <div class="center">
+        <asp:Panel ID="PanelBMProveedor" runat="server" Visible="false">
+            <h3>Modifica/Eliminar Proveedor</h3><br />
+            ID de Proveedor<br />
+            <asp:TextBox ID="txtModProvID" runat="server" Enabled="False" Width="107px"></asp:TextBox><br />
+            <br />
+            Nombre de la Empresa Proveedora<br />
+            <br />
+            <asp:TextBox ID="txtModNombreProv" runat="server"></asp:TextBox><br />
+            <br />
+            Nombre del contacto de la empresa<br />
+            <asp:TextBox ID="txtModContactoNombre" runat="server"></asp:TextBox><br />
+            <br />
+            CUIT<br />
+            <asp:TextBox ID="txtModCUIT" runat="server"></asp:TextBox><br />
+            <br />
+            Pais<br />
+            <asp:TextBox ID="txtModPais" runat="server"></asp:TextBox><br />
+            <br />
+            Ciudad<br />
+            <asp:TextBox ID="txtModCiudad" runat="server"></asp:TextBox><br />
+            <br />
+            Telefono<br />
+            <asp:TextBox ID="txtModTelefono" runat="server"></asp:TextBox><br />
+            <br />
+            <asp:Button ID="btnMinim" runat="server" Text="Minimizar" OnClick="btnMinim_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnModificar" runat="server" Text="Modificar" OnClick="btnModificar_Click" ValidationGroup="bm"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnEliminar" runat="server" Text="Eliminar" OnClick="btnEliminar_Click" />
+        </asp:Panel>
+
+
+    </div>
+    
+ 
+</asp:Content>
---
+++ Boxlit/Boxlit/clases/ABM_Proveedores.aspx.designer.cs
@@ -0,0 +1,213 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Boxlit.clases {
+    
+    
+    public partial class ABM_Proveedores {
+        
+        /// <summary>
+        /// Control btnMostrarPanelNuevoProv.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMostrarPanelNuevoProv;
+        
+        /// <summary>
+        /// Control PanelNuevoProveedor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PanelNuevoProveedor;
+        
+        /// <summary>
+        /// Control txtNombreProv.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreProv;
+        
+        /// <summary>
+        /// Control txtContactoNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtContactoNombre;
+        
+        /// <summary>
+        /// Control txtCUIT.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCUIT;
+        
+        /// <summary>
+        /// Control txtPais.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPais;
+        
+        /// <summary>
+        /// Control txtCiudad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCiudad;
+        
+        /// <summary>
+        /// Control txtTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelefono;
+        
+        /// <summary>
+        /// Control btnMinimizar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinimizar;
+        
+        /// <summary>
+        /// Control btnAgregar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregar;
+        
+        /// <summary>
+        /// Control GridViewProveedores.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridViewProveedores;
+        
+        /// <summary>
+        /// Control PanelBMProveedor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PanelBMProveedor;
+        
+        /// <summary>
+        /// Control txtModProvID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtModProvID;
+        
+        /// <summary>
+        /// Control txtModNombreProv.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtModNombreProv;
+        
+        /// <summary>
+        /// Control txtModContactoNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtModContactoNombre;
+        
+        /// <summary>
+        /// Control txtModCUIT.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtModCUIT;
+        
+        /// <summary>
+        /// Control txtModPais.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtModPais;
+        
+        /// <summary>
+        /// Control txtModCiudad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtModCiudad;
+        
+        /// <summary>
+        /// Control txtModTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtModTelefono;
+        
+        /// <summary>
+        /// Control btnMinim.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinim;
+        
+        /// <summary>
+        /// Control btnModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificar;
+        
+        /// <summary>
+        /// Control btnEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminar;
+    }
+}
